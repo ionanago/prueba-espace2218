c5868f5ece87ab2d880c4ede1d0a176d161b8d7a
pc <pc@DESKTOP-90ORF0R.home>
2019-07-24 19:49:29 +0200

    js mal nose arreglarlo

--- WebContent/js/js.js
+++ WebContent/js/js.js
@@ -59,31 +59,24 @@ $(function() {
 		// alert(usuario + password);
 	});
 	
-	$(".ModificarUsuarioServlet").click(function(e) {
+	$(".ModificarProductoServlet").click(function(e) {
 		e.preventDefault();
 		var padre = $(this).parent().parent();
-		var usuario = padre.find("td:first-child").text();
-		var password = padre.find("td:nth-child(2)").text();
-		if(usuario != "admin"){
-			$.post("ModificarUsuarioServlet", {
-			usuarionuevo : usuario,
-			passwordnuevo : password
+		var id = padre.find("td:first-child").text();
+		var nombre = padre.find("td:nth-child(2)").text();
+		var codigo  = padre.find("td:nth-child(3)").text();
+		var precio = padre.find("td:nth-child(4)").text();
+		$.post("ModificarProductoServlet", {
+			id:id,
+			nombre:nombre,
+			codigo:codigo,
+			precio:precio
 		}).done(function(e) {
-			
+			padre.remove();
 		}).fail(function(e) {
-		});}
-		else alert("No se puede modificar un admin!!")
-		
-		// alert(usuario + password);
-		
-	
-	
+		});
 	});
-	
-	
-	
-	
-});
+};
 function editar(t,e) {
 	e.preventDefault();
 	var padre = $(t).parent().parent(), i = 1;
