5f282ad3040c9f9298971796a1716fd6a2426f3c
ionanago <51776373+ionanago@users.noreply.github.com>
2019-07-24 08:53:04 +0200

    -->

---
+++ src/controladores/AñadirProductoServlet.java
@@ -0,0 +1,36 @@
+package controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelos.*;
+import negocio.*;
+import accessodatos.*;
+
+
+@WebServlet("/AñadirProductoServlet")
+public class AñadirProductoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public AñadirProductoServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
---
+++ src/controladores/AñadirUsuarioServlet.java
@@ -0,0 +1,44 @@
+package controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelos.*;
+import negocio.*;
+import accessodatos.*;
+
+/**
+ * Servlet implementation class AñadirUsuarioServlet
+ */
+@WebServlet("/AñadirUsuarioServlet")
+public class AñadirUsuarioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AñadirUsuarioServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
